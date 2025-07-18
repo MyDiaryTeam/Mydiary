1c8ed0987150d10d65e0768a9a63f3fb5433e388
hong-lahee <xhfxhfldwlsk@naver.com>
2025-07-18 18:31:03 +0900

    ⚡️ Diary_tags 코드 초기

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,24 @@
-from .models import User
+from tortoise import Tortoise
 
-__all__ = ["User"]  # import 할 클래스 빼줘야함 항상.
+TORTOISE_ORM = {
+    "connections": {
+        "default": "postgres://postgres:비밀번호@localhost:5432/my_diary"
+    },
+    "apps": {
+        "models": {
+            "models": [
+                "app.models.user",
+                "app.models.diary",
+                "app.models.tag",
+                "app.models.emotion_stats",
+                "app.models.alert_log",
+                "aerich.models"  # 마이그레이션용
+            ],
+            "default_connection": "default",
+        }
+    },
+}
+
+
+
+# __all__ = ["User"]  # import 할 클래스 빼줘야함 항상.
---
+++ app/services/diary_tags.py
@@ -0,0 +1,6 @@
+from app.models.diary_tags import DiaryTag
+
+
+class DiaryTagService:
+        async def get_tags_diary(self, diary_id: int) -> list:
+            pass
