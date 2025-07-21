5798d0f98d0a985918e854cf1d4dbd20e35ba1b2
hong-lahee <xhfxhfldwlsk@naver.com>
2025-07-21 13:33:25 +0900

    ⚡️ 오류수정

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,21 +1,3 @@
-from tortoise import Tortoise
+from .users import UserModel
 
-TORTOISE_ORM = {
-    "connections": {"default": "postgres://postgres:비밀번호@localhost:5432/my_diary"},
-    "apps": {
-        "models": {
-            "models": [
-                "app.models.user",
-                "app.models.diary",
-                "app.models.tag",
-                "app.models.emotion_stats",
-                "app.models.alert_log",
-                "aerich.models",  # 마이그레이션용
-            ],
-            "default_connection": "default",
-        }
-    },
-}
-
-
-# __all__ = ["User"]  # import 할 클래스 빼줘야함 항상.
+__all__ = ["UserModel"]  # import 할 클래스 빼줘야함 항상.
